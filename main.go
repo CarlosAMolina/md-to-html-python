10dca7c129c5fb46be6fee64ad87b264a38c0c6d
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2026-01-17 17:24:24 +0100

    feat(test local): add pull repo

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	fmt.Println("Welcome to the cmoli.es deployment CLI!")
 	showHelp()
+	testLocal() // TODO rm
+	os.Exit(0)  // TODO rm
 	var choice string
 	for {
 		fmt.Print(">> ")
@@ -16,10 +18,12 @@ func main() {
 		switch choice {
 		case "1":
 			fmt.Println("Starting full deployment")
-			run("ls -a -l -h")
 			os.Exit(0)
 		case "2":
 			fmt.Println("Generating web content")
+			os.Exit(0)
+		case "3":
+			testLocal()
 			os.Exit(0)
 		case "e":
 			fmt.Println("Bye!")
@@ -35,14 +39,25 @@ func showHelp() {
 	fmt.Println("Please select an option:")
 	fmt.Println("1. Full deployment. Run all steps")
 	fmt.Println("2. Generate web content. Convert Markdown to HTML")
+	fmt.Println("3. Testing local")
 	fmt.Println("e. Exit")
 	fmt.Println("h. Show help")
 }
 
 func run(command string) {
+	fmt.Println(command)
 	out, err := exec.Command("bash", "-c", command).Output()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(string(out))
+}
+
+func testLocal() {
+	fmt.Println("Testing local")
+	gitPull()
+}
+
+func gitPull() {
+	run("git pull origin $(git branch --show-current)")
 }
